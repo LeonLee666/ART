e5ff7e63c2d3070aa0ff96f9188b003251c751e8
bombel <liliangneu@126.com>
2019-01-05 20:46:01 +0800

    art in single thread and multi thread

---
+++ single_thread/app.c
@@ -0,0 +1,41 @@
+//
+// Created by bombehub on 2019/1/5.
+//
+
+#include <stdio.h>
+#include "art.h"
+#include <string.h>
+int main(int argc, char **argv) {
+    art_tree t;
+    int res = art_tree_init(&t);
+
+    int len;
+    char buf[512];
+    FILE *f = fopen("../dataset/words.txt", "r");
+
+    uintptr_t line = 1;
+    while (fgets(buf, sizeof buf, f)) {
+        len = strlen(buf);
+        buf[len - 1] = '\0';
+        art_insert(&t, (unsigned char *) buf, len, (void *) line);
+        //printf("%lld\n",art_size(&t) );
+        line++;
+    }
+
+
+    fseek(f, 0, SEEK_SET);
+    // Search for each line
+    line = 1;
+    while (fgets(buf, sizeof buf, f)) {
+        len = strlen(buf);
+        buf[len-1] = '\0';
+
+        uintptr_t val = (uintptr_t)art_search(&t, (unsigned char*)buf, len);
+        printf("Line: %d Val: %d Str: %s\n", line,val, buf);
+        line++;
+    }
+
+
+    res = art_tree_destroy(&t);
+
+}
